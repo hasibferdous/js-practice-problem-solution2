e3165bcd8617ff02da3b39e4f2abf2b88b1d0429
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-09 01:29:38 +0600

    problem solving

--- problemsolving.js
+++ problemsolving.js
@@ -49,3 +49,39 @@ console.log('total page required:', totalPage);
 //----------------------------------------------
 //bestFriend
 //----------------------------------------------
+
+friend = ['ashik','shakil','kausar','jahid','shuvo'];
+function bestFriend(arra){
+
+    var max_str = arra[0].length;
+    var ans = arra[0];
+    for (var i = 1; i < arra.length; i++) {
+        var maxi = arra[i].length;
+        if (maxi > max_str) {
+            ans = arra[i];
+            max_str = maxi;
+        }
+    }
+    return ans;
+}
+let friends = bestFriend(friend);
+console.log('longest string:', friends);
+
+//----------------------------------------------
+//positive integer
+//----------------------------------------------
+const number = [2,4,-5,6,-4,2,-7,6,8,-9];
+
+function removeNegative(number){
+    const unique = [];
+    for(let i=0; i< number.length; i++){
+        const numbers = number[i];
+        if(numbers >= 0 ){
+            unique.push(numbers);
+       }
+        
+        }
+        return unique;
+    }
+const uniqueNumbers = removeNegative(number);
+console.log('The array without negative numbers:', uniqueNumbers);
