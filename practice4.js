92191e8aa49f948a05de3ac45b1bc0c32083b8d3
hasibferdous <hasibferdous.ict@gmail.com>
2022-08-08 23:06:41 +0600

    practice

--- practice4.js
+++ practice4.js
@@ -141,3 +141,38 @@ console.log("total quantity Expense :", qunatityExpense);
 
 
 //-------------------------------------------
+
+//discount
+//1. if ticket number is less than 100,then per ticket price: 100
+//2. if ticket number is more than 100 but less than 200,then first 100 ticket price: 100, rest ticket price will be 90 taka per piece
+//3. if you purchase more than 200 tickets
+    //first 100-- 100 taka
+    //101-200 -- 90 taka
+    //200+ -- 70 taka
+
+function ticketPrice(ticketQuantity){
+    const first100Rate = 100;
+    const second100Rate = 90;
+    const restTicketRate = 70;
+    if(ticketQuantity <= 100){
+        const price = ticketQuantity * first100Rate;
+        return price;
+    }
+    else if(ticketQuantity <= 200){
+        const first100Price = 100* first100Rate;
+        const restTicketQuantity = ticketQuantity - 100;
+        const restTicketPrice = restTicketQuantity * second100Rate;
+        const totalPrice = first100Price + restTicketPrice;
+        return totalPrice;
+    }
+    else{
+        const first100Price = 100* first100Rate;
+        const second100Price = 100* second100Rate;
+        const restTicketQuantity = ticketQuantity - 200;
+        const restTicketPrice = restTicketQuantity* restTicketRate;
+        const totalPrice = first100Price + second100Price + restTicketPrice;
+        return totalPrice;
+    }
+}
+const price = ticketPrice(126);
+console.log('price:', price);
